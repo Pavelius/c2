66c14aea874d38b4dc838ee3c8bcc05eed60e4fc
Pavel Chistyakov <p000000000001@gmail.com>
2017-08-13 10:08:55 +0300

    Independed execution

--- source/files.h
+++ source/files.h
@@ -6,6 +6,7 @@ namespace c2
 	{
 		extern const char*	library;
 		extern const char*	project;
+		extern const char*	platform;
 	}
 	char*					getfilename(char* result, const char* url); // Get valid name of file bt short path
 	const char*				getfile(const char* id); // Get content of file with specified url's id like 'io.base' or 'acces.file'
--- source/main.cpp
+++ source/main.cpp
@@ -29,20 +29,21 @@ static void status(c2::messages m, const c2::type* module, const c2::type* membe
 
 int main(int argc, char *argv[])
 {
-	//if(argc<2)
-	//{
-	//	printc("C2 compiler. Copyright (c) by Pavel Chistyakov, 2015\n");
-	//	printc("Options:\n");
-	//	printc("-I	specified library path (like -Id:/libs)\n");
-	//	printc("-C	specified compiled source path (like -Cd:/projects/test)\n");
-	//	printc("-P	specified platform source path (like -Pc:/windows/system32)\n");
-	//	printc("-O	specified output file name (like -Od:/projects/test/test.exe)\n");
+	if(argc<2)
+	{
+		printc("C2 compiler. Copyright (c) by Pavel Chistyakov, 2015-2017\n");
+		printc("Options:\n");
+		printc("-I	specified library path (like -Id:/libs)\n");
+		printc("-C	specified compiled source path (like -Cd:/projects/test)\n");
+		printc("-P	specified platform source path (like -Pc:/windows/system32)\n");
+		printc("-O	specified output file name (like -Od:/projects/test/test.exe)\n");
 	//	return 0;
-	//}
+	}
 	c2::errorproc = error;
 	c2::statusproc = status;
-	c2::urls::project = "D:/projects/c2code/projects/anytest";
-	c2::urls::library = "D:/projects/c2code/library";
+	c2::urls::project = "projects/anytest";
+	c2::urls::library = "library";
+	c2::urls::platform = "C:/windows/system32";
 	c2::type::cleanup();
 	c2::type::compile(szdup("main"));
 	return 0;
